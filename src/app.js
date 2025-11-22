b5d16a26e13f6671177cd0782d40c37b4cf029f7
hrishikesh-madgundi <madgundi.hrishikesh@webseeder.tech>
2025-11-23 00:26:29 +0530

    server update

--- src/app.js
+++ src/app.js
@@ -12,9 +12,10 @@ const PORT = process.env.PORT || 6000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/", ( req, res )=>{ res.send("Server is running fine") })
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
+
+app.use("/", ( req, res )=>{ res.send("Server is running fine") })
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on the port ${PORT}`)
